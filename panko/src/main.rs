3a4e7f53295a02c0c8ec8e1c05d6d8202a855197
Paul Gey <narpfel@gmx.de>
2024-08-02 18:42:03 +0200

    driver: read source filename from command line args

--- panko/src/main.rs
+++ panko/src/main.rs
@@ -1,14 +1,21 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 use bumpalo::Bump;
+use clap::Parser;
 use panko_parser::sexpr_builder::AsSExpr as _;
 
+#[derive(Debug, Parser)]
+struct Args {
+    filename: PathBuf,
+}
+
 fn main() {
+    let args = Args::parse();
     let bump = &Bump::new();
     let tokens = panko_lex::lex(
         bump,
-        Path::new("main.c"),
-        &std::fs::read_to_string("main.c").unwrap(),
+        &args.filename,
+        &std::fs::read_to_string(&args.filename).unwrap(),
     );
     let translation_unit = panko_parser::parse(bump, tokens).unwrap();
     let translation_unit = panko_sema::resolve_names(bump, translation_unit);
