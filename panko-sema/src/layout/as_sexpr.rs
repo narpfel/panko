861777eee0cc23fdef80c598afd5b20b1abd801b
Paul Gey <narpfel@gmx.de>
2024-09-24 20:08:20 +0200

    make layout sexpr repr of assignment exprs more readable

--- panko-sema/src/layout/as_sexpr.rs
+++ panko-sema/src/layout/as_sexpr.rs
@@ -95,8 +95,7 @@ impl AsSExpr for Expression<'_> {
             Expression::Truncate(from) => SExpr::new("truncate").inherit(from),
             Expression::SignExtend(from) => SExpr::new("sign-extend").inherit(from),
             Expression::ZeroExtend(from) => SExpr::new("zero-extend").inherit(from),
-            Expression::Assign { target, value } =>
-                SExpr::new("assign").inherit(target).inherit(value),
+            Expression::Assign { target, value } => SExpr::new("assign").lines([target, value]),
             Expression::IntegralBinOp { ty: _, lhs, kind, rhs } =>
                 SExpr::new(kind.str()).lines([lhs, rhs]),
             Expression::PtrAdd {
