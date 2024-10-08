2174091ed997b668f261ae1b444345a9f54e45c3
Paul Gey <narpfel@gmx.de>
2024-10-08 18:52:01 +0200

    add more zero-extend tests

--- panko/tests/cases/execute/test_zero_extend.c
+++ panko/tests/cases/execute/test_zero_extend.c
@@ -7,25 +7,53 @@ void print(char** argv, unsigned long l) {
 }
 
 int main(int, char** argv) {
-    char unsigned c = 42;
-    // [[print: 42]]
-    print(argv, c);
-    unsigned short s = c;
-    // [[print: 42]]
-    print(argv, s);
-    unsigned int i = c;
-    // [[print: 42]]
-    print(argv, i);
-    unsigned int j = s;
-    // [[print: 42]]
-    print(argv, j);
-    unsigned long l = c;
-    // [[print: 42]]
-    print(argv, l);
-    unsigned long m = s;
-    // [[print: 42]]
-    print(argv, m);
-    unsigned long n = i;
-    // [[print: 42]]
-    print(argv, n);
+    // zero in msb
+    {
+        char unsigned c = 42;
+        // [[print: 42]]
+        print(argv, c);
+        unsigned short s = c;
+        // [[print: 42]]
+        print(argv, s);
+        unsigned int i = c;
+        // [[print: 42]]
+        print(argv, i);
+        unsigned int j = s;
+        // [[print: 42]]
+        print(argv, j);
+        unsigned long l = c;
+        // [[print: 42]]
+        print(argv, l);
+        unsigned long m = s;
+        // [[print: 42]]
+        print(argv, m);
+        unsigned long n = i;
+        // [[print: 42]]
+        print(argv, n);
+    }
+
+    // one in msb
+    {
+        char unsigned c = 250;
+        // [[print: 250]]
+        print(argv, c);
+        unsigned short s = c;
+        // [[print: 250]]
+        print(argv, s);
+        unsigned int i = c;
+        // [[print: 250]]
+        print(argv, i);
+        unsigned int j = s;
+        // [[print: 250]]
+        print(argv, j);
+        unsigned long l = c;
+        // [[print: 250]]
+        print(argv, l);
+        unsigned long m = s;
+        // [[print: 250]]
+        print(argv, m);
+        unsigned long n = i;
+        // [[print: 250]]
+        print(argv, n);
+    }
 }
