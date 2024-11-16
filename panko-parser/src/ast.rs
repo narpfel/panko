bf62a7e0ba38a020632e6a25f90b8b767458b5ac
Paul Gey <narpfel@gmx.de>
2024-11-16 20:16:08 +0100

    ast: remove unnecessary `PartialEq` and `Eq` instances from `ast::Type` and related types

--- panko-parser/src/ast.rs
+++ panko-parser/src/ast.rs
@@ -162,8 +162,6 @@ pub struct FunctionDefinition<'a> {
     pub body: CompoundStatement<'a>,
 }
 
-// TODO: The instances for `PartialEq` and `Eq` are only needed until correct type checking is
-// implemented and should be removed afterwards.
 #[derive(Debug, Clone, Copy)]
 pub struct QualifiedType<'a> {
     pub is_const: bool,
@@ -172,7 +170,7 @@ pub struct QualifiedType<'a> {
     pub loc: Loc<'a>,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy)]
 pub enum Type<'a> {
     Arithmetic(Arithmetic),
     Pointer(&'a QualifiedType<'a>),
@@ -209,7 +207,7 @@ pub enum Signedness {
     Unsigned,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy)]
 pub struct FunctionType<'a> {
     pub params: &'a [ParameterDeclaration<'a>],
     pub return_type: &'a QualifiedType<'a>,
@@ -222,14 +220,6 @@ pub struct ParameterDeclaration<'a> {
     pub ty: QualifiedType<'a>,
     pub name: Option<Token<'a>>,
 }
-
-impl PartialEq for ParameterDeclaration<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.ty == other.ty
-    }
-}
-
-impl Eq for ParameterDeclaration<'_> {}
 
 #[derive(Debug, Clone, Copy)]
 pub struct CompoundStatement<'a>(pub &'a [Statement<'a>]);
@@ -280,21 +270,6 @@ impl<'a> FunctionDefinition<'a> {
         }
     }
 }
-
-impl PartialEq for QualifiedType<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        let Self { is_const, is_volatile, ty, loc: _ } = self;
-        let Self {
-            is_const: other_is_const,
-            is_volatile: other_is_volatile,
-            ty: other_ty,
-            loc: _,
-        } = other;
-        (is_const, is_volatile, ty) == (other_is_const, other_is_volatile, other_ty)
-    }
-}
-
-impl Eq for QualifiedType<'_> {}
 
 impl fmt::Display for QualifiedType<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
