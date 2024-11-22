de9ce9faa791f9277ff3ce14b41a128b7848ca92
Paul Gey <narpfel@gmx.de>
2024-11-22 19:33:34 +0100

    sema: use better name for generic parameter of `ty::Type` that represents array lengths

--- panko-sema/src/ty.rs
+++ panko-sema/src/ty.rs
@@ -17,13 +17,13 @@ use panko_parser::NO_VALUE;
 use crate::typecheck::ArrayLength;
 
 #[derive(Debug, Clone, Copy)]
-pub struct ParameterDeclaration<'a, TypeofExpr, Expression> {
+pub struct ParameterDeclaration<'a, TypeofExpr, LengthExpr> {
     pub loc: Loc<'a>,
-    pub ty: QualifiedType<'a, TypeofExpr, Expression>,
+    pub ty: QualifiedType<'a, TypeofExpr, LengthExpr>,
     pub name: Option<Token<'a>>,
 }
 
-impl<'a, TypeofExpr, Expression> ParameterDeclaration<'a, TypeofExpr, Expression> {
+impl<'a, TypeofExpr, LengthExpr> ParameterDeclaration<'a, TypeofExpr, LengthExpr> {
     pub(crate) fn loc(&self) -> Loc<'a> {
         self.loc
     }
@@ -31,7 +31,7 @@ impl<'a, TypeofExpr, Expression> ParameterDeclaration<'a, TypeofExpr, Expression
     pub(crate) fn slice(&self) -> Cow<'a, str>
     where
         TypeofExpr: AsSExpr,
-        Expression: AsSExpr,
+        LengthExpr: AsSExpr,
     {
         self.name
             .map(|name| Cow::Borrowed(name.slice()))
@@ -39,27 +39,27 @@ impl<'a, TypeofExpr, Expression> ParameterDeclaration<'a, TypeofExpr, Expression
     }
 }
 
-impl<TypeofExpr, Expression> PartialEq for ParameterDeclaration<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> PartialEq for ParameterDeclaration<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: PartialEq,
-    Expression: PartialEq,
+    LengthExpr: PartialEq,
 {
     fn eq(&self, other: &Self) -> bool {
         self.ty.ty == other.ty.ty
     }
 }
 
-impl<TypeofExpr, Expression> Eq for ParameterDeclaration<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> Eq for ParameterDeclaration<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: Eq,
-    Expression: Eq,
+    LengthExpr: Eq,
 {
 }
 
-impl<TypeofExpr, Expression> Hash for ParameterDeclaration<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> Hash for ParameterDeclaration<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: Hash,
-    Expression: Hash,
+    LengthExpr: Hash,
 {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.ty.ty.hash(state)
@@ -67,15 +67,15 @@ where
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ArrayType<'a, TypeofExpr, Expression> {
-    pub ty: &'a QualifiedType<'a, TypeofExpr, Expression>,
-    pub length: &'a Expression,
+pub struct ArrayType<'a, TypeofExpr, LengthExpr> {
+    pub ty: &'a QualifiedType<'a, TypeofExpr, LengthExpr>,
+    pub length: &'a LengthExpr,
 }
 
-impl<TypeofExpr, Expression> fmt::Display for ArrayType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> fmt::Display for ArrayType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: AsSExpr,
-    Expression: AsSExpr,
+    LengthExpr: AsSExpr,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Self { ty, length } = self;
@@ -84,16 +84,16 @@ where
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct FunctionType<'a, TypeofExpr, Expression> {
-    pub params: &'a [ParameterDeclaration<'a, TypeofExpr, Expression>],
-    pub return_type: &'a QualifiedType<'a, TypeofExpr, Expression>,
+pub struct FunctionType<'a, TypeofExpr, LengthExpr> {
+    pub params: &'a [ParameterDeclaration<'a, TypeofExpr, LengthExpr>],
+    pub return_type: &'a QualifiedType<'a, TypeofExpr, LengthExpr>,
     pub is_varargs: bool,
 }
 
-impl<TypeofExpr, Expression> fmt::Display for FunctionType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> fmt::Display for FunctionType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: AsSExpr,
-    Expression: AsSExpr,
+    LengthExpr: AsSExpr,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Self { params, return_type, is_varargs } = *self;
@@ -117,17 +117,17 @@ where
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Type<'a, TypeofExpr, Expression> {
+pub enum Type<'a, TypeofExpr, LengthExpr> {
     Arithmetic(Arithmetic),
-    Pointer(&'a QualifiedType<'a, TypeofExpr, Expression>),
-    Array(ArrayType<'a, TypeofExpr, Expression>),
-    Function(FunctionType<'a, TypeofExpr, Expression>),
+    Pointer(&'a QualifiedType<'a, TypeofExpr, LengthExpr>),
+    Array(ArrayType<'a, TypeofExpr, LengthExpr>),
+    Function(FunctionType<'a, TypeofExpr, LengthExpr>),
     Void,
     Typeof { expr: TypeofExpr, unqual: bool },
     // TODO
 }
 
-impl<'a, TypeofExpr, Expression> Type<'a, TypeofExpr, Expression> {
+impl<'a, TypeofExpr, LengthExpr> Type<'a, TypeofExpr, LengthExpr> {
     pub const fn uchar() -> Self {
         Self::Arithmetic(Arithmetic::Integral(Integral {
             signedness: Signedness::Unsigned,
@@ -166,10 +166,10 @@ impl<'a, TypeofExpr, Expression> Type<'a, TypeofExpr, Expression> {
         }))
     }
 
-    pub fn unqualified(&self) -> QualifiedType<'a, TypeofExpr, Expression>
+    pub fn unqualified(&self) -> QualifiedType<'a, TypeofExpr, LengthExpr>
     where
         TypeofExpr: Copy,
-        Expression: Copy,
+        LengthExpr: Copy,
     {
         QualifiedType {
             is_const: false,
@@ -184,7 +184,7 @@ impl<'a, TypeofExpr, Expression> Type<'a, TypeofExpr, Expression> {
     pub fn slice(&self) -> String
     where
         TypeofExpr: AsSExpr,
-        Expression: AsSExpr,
+        LengthExpr: AsSExpr,
     {
         self.to_string()
     }
@@ -205,7 +205,7 @@ impl<'a, TypeofExpr, Expression> Type<'a, TypeofExpr, Expression> {
     }
 }
 
-impl<Expression> Type<'_, !, ArrayLength<Expression>> {
+impl<LengthExpr> Type<'_, !, ArrayLength<LengthExpr>> {
     pub fn size(&self) -> u64 {
         match self {
             Type::Arithmetic(arithmetic) => arithmetic.size(),
@@ -254,10 +254,10 @@ impl<Expression> Type<'_, !, ArrayLength<Expression>> {
     }
 }
 
-impl<TypeofExpr, Expression> fmt::Display for Type<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> fmt::Display for Type<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: AsSExpr,
-    Expression: AsSExpr,
+    LengthExpr: AsSExpr,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -272,20 +272,20 @@ where
 }
 
 #[derive(Debug, Clone, Copy)]
-pub struct QualifiedType<'a, TypeofExpr, Expression> {
+pub struct QualifiedType<'a, TypeofExpr, LengthExpr> {
     pub(crate) is_const: bool,
     pub(crate) is_volatile: bool,
-    pub ty: Type<'a, TypeofExpr, Expression>,
+    pub ty: Type<'a, TypeofExpr, LengthExpr>,
     pub(crate) loc: Loc<'a>,
 }
 
-impl<TypeofExpr, Expression> QualifiedType<'_, TypeofExpr, Expression> {
+impl<TypeofExpr, LengthExpr> QualifiedType<'_, TypeofExpr, LengthExpr> {
     // TODO: this is a temporary hack until the `Report` derive macro handles stringification
     // better
     pub(crate) fn slice(&self) -> String
     where
         TypeofExpr: AsSExpr,
-        Expression: AsSExpr,
+        LengthExpr: AsSExpr,
     {
         self.to_string()
     }
@@ -295,10 +295,10 @@ impl<TypeofExpr, Expression> QualifiedType<'_, TypeofExpr, Expression> {
     }
 }
 
-impl<TypeofExpr, Expression> PartialEq for QualifiedType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> PartialEq for QualifiedType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: PartialEq,
-    Expression: PartialEq,
+    LengthExpr: PartialEq,
 {
     fn eq(&self, other: &Self) -> bool {
         let Self { is_const, is_volatile, ty, loc: _ } = self;
@@ -312,17 +312,17 @@ where
     }
 }
 
-impl<TypeofExpr, Expression> Eq for QualifiedType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> Eq for QualifiedType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: Eq,
-    Expression: Eq,
+    LengthExpr: Eq,
 {
 }
 
-impl<TypeofExpr, Expression> Hash for QualifiedType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> Hash for QualifiedType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: Hash,
-    Expression: Hash,
+    LengthExpr: Hash,
 {
     fn hash<H: Hasher>(&self, state: &mut H) {
         let Self { is_const, is_volatile, ty, loc: _ } = self;
@@ -330,10 +330,10 @@ where
     }
 }
 
-impl<TypeofExpr, Expression> fmt::Display for QualifiedType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> fmt::Display for QualifiedType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: AsSExpr,
-    Expression: AsSExpr,
+    LengthExpr: AsSExpr,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.ty.fmt(f)?;
@@ -347,10 +347,10 @@ where
     }
 }
 
-impl<TypeofExpr, Expression> AsSExpr for QualifiedType<'_, TypeofExpr, Expression>
+impl<TypeofExpr, LengthExpr> AsSExpr for QualifiedType<'_, TypeofExpr, LengthExpr>
 where
     TypeofExpr: AsSExpr,
-    Expression: AsSExpr,
+    LengthExpr: AsSExpr,
 {
     fn as_sexpr(&self) -> SExpr {
         SExpr::display(self)
