5985ddb090011e7cd0ac2e0392e2259f814f8dbc
Paul Gey <narpfel@gmx.de>
2024-11-23 21:30:43 +0100

    implement parsing simple `array-abstract-declarator`s

--- panko/tests/cases/execute/arrays/test_multidimensional_array_length.c
+++ panko/tests/cases/execute/arrays/test_multidimensional_array_length.c
@@ -4,6 +4,8 @@
 int printf(char const*, ...);
 
 int main(int, char** argv) {
+    // [[print: 100]]
+    printf(argv[1], _Lengthof(long*[100][400]));
     int xs[42][27];
     // [[print: 42]]
     printf(argv[1], _Lengthof xs);
--- panko/tests/cases/execute/arrays/test_multidimensional_array_size.c
+++ panko/tests/cases/execute/arrays/test_multidimensional_array_size.c
@@ -1,6 +1,11 @@
 // [[return: 60]]
+// [[arg: %zu\n]]
 
-int main() {
+int printf(char const*, ...);
+
+int main(int, char** argv) {
+    // [[print: 400000]]
+    printf(argv[1], sizeof(int[200][500]));
     int xs[5][3];
     return sizeof xs;
 }
