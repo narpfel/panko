67ddfa6460bb920c83709d57bd85d6e5608de213
Paul Gey <narpfel@gmx.de>
2025-02-23 20:37:10 +0100

    add tests for excess empty braces in initialiser

--- panko/tests/cases/initialisation/test_excess_initialisers_braced.c
+++ panko/tests/cases/initialisation/test_excess_initialisers_braced.c
@@ -6,4 +6,8 @@ int main() {
     int zs[3][2] = {{1, 2, {3, 6}}, {4, 5}};
     int qs[2] = {1, 2, {3, 4}, 5};
     int ws[3][2] = {{1, 2, {3, 6}, 70, 80}, {4, 5}, 6, 7, 8, 9};
+
+    int excess_empty_braces_in_scalar = {42, {}};
+    int excess_empty_braces_in_array[2] = {1, 2, {}};
+    int excess_empty_braces_in_array_2[2] = {1, 2, {}, 4};
 }
