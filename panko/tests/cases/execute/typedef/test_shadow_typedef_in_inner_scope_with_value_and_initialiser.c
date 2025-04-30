4ddc79266ee714bbdf354cc057ba62c412f02b69
Paul Gey <narpfel@gmx.de>
2025-04-30 20:28:46 +0200

    implement initialisation of variable shadowing a `typedef`’d name

---
+++ panko/tests/cases/execute/typedef/test_shadow_typedef_in_inner_scope_with_value_and_initialiser.c
@@ -0,0 +1,19 @@
+// [[return: 42]]
+
+int printf(char const*, ...);
+
+int main() {
+    {
+        typedef int name;
+        {
+            long name = -42;
+            // [[print: -42]]
+            printf("%ld\n", name);
+        }
+        name x = 123;
+        // [[print: 123]]
+        printf("%d\n", x);
+    }
+    int name = 42;
+    return name;
+}
