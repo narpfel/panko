002c640d51909e2c5ee296830f6807db3716f52e
Paul Gey <narpfel@gmx.de>
2025-05-03 21:14:17 +0200

    improve test case for function type with type qualifiers

--- panko/tests/cases/decl_in_global_scope/test_error_function_type_is_qualified.c
+++ panko/tests/cases/decl_in_global_scope/test_error_function_type_is_qualified.c
@@ -1,9 +1,14 @@
-// TODO: 6.7.4.1 sentence 10 defines the behaviour of a function type with any
-// type qualifiers as undefined. We generate a nonsensical error message in this
-// case that should be improved.
-
 typedef int F();
 
+// 6.7.4.1 sentence 10 defines the behaviour of a function type with any
+// type qualifiers as undefined. We currently accept this.
+
+// TODO: This should error.
 F const f;
 
-int const f() {}
+
+F const g;
+
+// TODO: We generate a nonsensical error message in this
+// case that should be improved.
+int const g() {}
