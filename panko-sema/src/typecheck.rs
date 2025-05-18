narpfel/panko

commit 36af9237ab5951b90e6ed6253dfe06617d86cfef

lint against `as` conversions

panko-sema/src/typecheck.rs
@@ -2056,7 +2056,10 @@ impl Char {
                 }
             }
             Char::EscapeSequence(value) => {
-                yield value as u8;
+                yield value.try_into().expect(
+                    "TODO: only unprefixed string literals are implemented; non-`u8` escape \
+                    sequences are only allowed for prefixed string literals",
+                );
             }
         };
     }

Cargo.toml
@@ -17,6 +17,7 @@ unqualified_local_imports = "warn"
 unused_qualifications = "warn"
 
 [workspace.lints.clippy]
+as_conversions = "warn"
 # TODO: remove this and `expect` the warning for each enum separately
 large_enum_variant = "allow"
 

panko-codegen/src/lib.rs
@@ -54,6 +54,10 @@ mod lineno;
 mod operand;
 
 const MAX_IMUL_IMMEDIATE: u64 = 2_u64.pow(31);
+#[expect(
+    clippy::as_conversions,
+    reason = "conversion using `From` not allowed in `const`"
+)]
 const MAX_ADDRESS_OFFSET: u64 = u32::MAX as _;
 const ARGUMENT_REGISTERS: [Register; 6] = [Rdi, Rsi, Rdx, Rcx, R8, R9];
 

panko-parser/src/lib.rs
@@ -34,6 +34,7 @@ pub mod ast;
 pub mod sexpr_builder;
 
 lalrpop_mod!(
+    #[allow(clippy::as_conversions, reason = "lalrpop generates `as` conversions")]
     #[allow(clippy::uninlined_format_args)]
     #[allow(unused_qualifications)]
     grammar

commit ad110460a1551b7fb55d63ee446fa42ea9ab9a79

fix non-ASCII escape sequences in string literals
broken by 36af9237ab5951b90e6ed6253dfe06617d86cfef

panko-sema/src/typecheck.rs
@@ -2056,10 +2056,14 @@ impl Char {
                 }
             }
             Char::EscapeSequence(value) => {
-                yield value.try_into().expect(
-                    "TODO: only unprefixed string literals are implemented; non-`u8` escape \
-                    sequences are only allowed for prefixed string literals",
-                );
+                #[expect(
+                    clippy::as_conversions,
+                    reason = "\
+                        this should truncate; the value is checked to be in range for \
+                        non-prefixed string literals in `parse_escape_sequences`\
+                    "
+                )]
+                yield value as u8;
             }
         };
     }