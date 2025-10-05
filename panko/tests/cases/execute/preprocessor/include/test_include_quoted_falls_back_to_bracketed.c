5a65fe04d1fad145f6b0d5c7ab861ab56d0ea4ed
Paul Gey <narpfel@gmx.de>
2025-10-05 14:40:09 +0200

    don’t `#include <assert.h>` in snapshot tests because the content of external system headers is not stable

--- panko/tests/cases/execute/preprocessor/include/test_include_quoted_falls_back_to_bracketed.c
+++ panko/tests/cases/execute/preprocessor/include/test_include_quoted_falls_back_to_bracketed.c
@@ -1,11 +1,9 @@
 // [[return: 1]]
 
-#include "assert.h"
 #include "iso646.h"
 #include "stddef.h"
 
 int main() {
     int xs[] = {1, 2};
-    assert(_Generic(sizeof 42, size_t: 0) or _Generic(&xs[1] - &xs[0], ptrdiff_t: 1));
     return _Generic(sizeof 42, size_t: 0) or _Generic(&xs[1] - &xs[0], ptrdiff_t: 1);
 }
