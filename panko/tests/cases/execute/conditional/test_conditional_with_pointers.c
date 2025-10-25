7b5fdad6e51ac9c29bbb9ee3cbec122619112f65
Paul Gey <narpfel@gmx.de>
2025-10-25 21:48:36 +0200

    add test for conditional with pointer and `nullptr`

--- panko/tests/cases/execute/conditional/test_conditional_with_pointers.c
+++ panko/tests/cases/execute/conditional/test_conditional_with_pointers.c
@@ -8,6 +8,7 @@ int main() {
     _Generic(0 ? c_vp : c_ip, void const*: 0);
     // TODO: nullptr constants
     // _Generic(0 ? v_ip : 0, int volatile*: 0);
+    _Generic(0 ? v_ip : nullptr, int volatile*: 0);
     _Generic(0 ? c_ip : v_ip, int const volatile*: 0);
     _Generic(0 ? vp : c_cp, void const*: 0);
     _Generic(0 ? ip : c_ip, int const*: 0);
