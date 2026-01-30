ed832c47a16e80c00e86dc1aa8e84e1cb5ae5096
Paul Gey <narpfel@gmx.de>
2026-01-30 22:23:30 +0100

    fix wrong variable name introduced by copy-pasting the test case

--- panko/tests/cases/execute/initialisation/struct/test_braced_init_of_struct.c
+++ panko/tests/cases/execute/initialisation/struct/test_braced_init_of_struct.c
@@ -8,12 +8,12 @@ struct T {
 };
 
 int main() {
-    struct T zeroed_t = {0x1234'5678, 1, 2, 3, 4, 0xabcd'ef42};
-    char bytes[sizeof zeroed_t] = {
+    struct T t = {0x1234'5678, 1, 2, 3, 4, 0xabcd'ef42};
+    char bytes[sizeof t] = {
         0x78, 0x56, 0x34, 0x12,
         1, 2, 3, 4,
         0x42, 0xef, 0xcd, 0xab,
         0, 0, 0, 0,
     };
-    return memcmp(&zeroed_t, bytes, sizeof zeroed_t);
+    return memcmp(&t, bytes, sizeof t);
 }
