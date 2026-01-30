ed87cfa6e5214df287a5bde84861409cf5bf6db1
Paul Gey <narpfel@gmx.de>
2026-01-30 20:04:14 +0100

    add `known-bug` tests for braced initialisation of nested struct with string literal

---
+++ panko/tests/cases/execute/initialisation/struct/test_nested_struct_with_braced_string_literal.c
@@ -0,0 +1,20 @@
+// [[known-bug]]
+// [[nosnapshot]]
+
+int printf(char const*, ...);
+
+struct String {
+    char const* ptr;
+};
+
+struct T {
+    struct String string;
+    int x;
+};
+
+int main() {
+    struct T t = {{"hello world"}, 42};
+
+    // [[print: hello world]]
+    printf("%s\n", *(char const**)&t);
+}
