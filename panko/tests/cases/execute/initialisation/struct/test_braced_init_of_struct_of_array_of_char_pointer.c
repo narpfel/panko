7a9d3edab4c64f519c10e9cf8ff3804dac869be7
Paul Gey <narpfel@gmx.de>
2026-01-31 22:23:25 +0100

    add test for braced initialisation of struct of array of char ptr with string literal

---
+++ panko/tests/cases/execute/initialisation/struct/test_braced_init_of_struct_of_array_of_char_pointer.c
@@ -0,0 +1,11 @@
+// [[known-bug]]
+
+int printf(char const*, ...);
+
+int main() {
+    struct T {
+        typeof(char const*) s[1];
+    } t = {"hello world"};
+    // [[print: hello world]]
+    printf("%s\n", *(char const**)&t);
+}
