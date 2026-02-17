4f83f06185d7cb4245b9e805289b6f72c96ccff0
Paul Gey <narpfel@gmx.de>
2026-02-17 21:59:30 +0100

    add test for reading struct member

---
+++ panko/tests/cases/execute/struct/test_read_member.c
@@ -0,0 +1,51 @@
+int printf(char const*, ...);
+
+struct Inner {
+    int x;
+    int y;
+    int z;
+};
+
+struct Middle {
+    int y;
+    struct Inner inner;
+    struct Inner inner2;
+};
+
+struct Outer {
+    int x;
+    struct Middle middle;
+    struct Inner inner;
+    int y;
+};
+
+void print_inner(struct Inner* p) {
+    printf("Inner { .x = %d, .y = %d, .z = %d }\n", p->x, p->y, p->z);
+}
+
+int main() {
+    struct Inner
+        inner = {1, 2, 3},
+        inner2 = {4, 5, 6},
+        inner3 = {inner.x + 10, inner2.y + 20, inner2.z + 30};
+    struct Middle middle = {2742, inner, inner2};
+    struct Outer outer = {
+        42,
+        middle,
+        inner3,
+        27,
+    };
+
+    // [[print: Inner { .x = 1, .y = 2, .z = 3 }]]
+    print_inner(&inner);
+    // [[print: Inner { .x = 4, .y = 5, .z = 6 }]]
+    print_inner(&outer.middle.inner2);
+    // [[print: Inner { .x = 11, .y = 25, .z = 36 }]]
+    print_inner(&outer.inner);
+
+    // [[print: 27 2742 6]]
+    printf("%d %d %d\n", outer.y, outer.middle.y, outer.middle.inner2.z);
+
+    // [[print: 16]]
+    printf("%td\n", (char*)&outer.middle.inner2.y - (char*)&outer.middle.inner);
+}
