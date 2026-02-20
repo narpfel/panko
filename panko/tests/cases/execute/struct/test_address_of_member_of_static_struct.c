d15d3e20712d54d78affeb99b331ec8567ab8be0
Paul Gey <narpfel@gmx.de>
2026-02-20 22:11:37 +0100

    add `known-bug` test for taking address of member of struct with static storage duration

---
+++ panko/tests/cases/execute/struct/test_address_of_member_of_static_struct.c
@@ -0,0 +1,14 @@
+// [[known-bug]]
+// [[return: 27]]
+
+struct T {
+    int x;
+    int y;
+};
+
+struct T static_struct = {42, 27};
+
+int main() {
+    int* p = &static_struct.y;
+    return *p;
+}
