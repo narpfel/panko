41bde50261f0e79435f59ce859a9a89125062e34
Paul Gey <narpfel@gmx.de>
2026-02-28 21:49:28 +0100

    add `known-bug` test for passing multiple structs in registers

---
+++ panko/tests/cases/execute/struct/abi/test_multiple_structs_passed_as_int_pair.c
@@ -0,0 +1,22 @@
+// [[known-bug]]
+
+int printf(char const*, ...);
+
+struct Struct {
+    char string[11];
+};
+
+void print_struct(char x, struct Struct value, char y, struct Struct other, char z) {
+    printf("%s\n", value.string);
+    printf("%s\n", other.string);
+    printf("%d %d %d\n", x, y, z);
+}
+
+int main() {
+    struct Struct value = {"0123456789"};
+    struct Struct v2 = {"abcdefghjk"};
+    // [[print: 0123456789]]
+    // [[print: abcdefghjk]]
+    // [[print: 123 42 27]]
+    print_struct(123, value, 42, v2, 27);
+}
