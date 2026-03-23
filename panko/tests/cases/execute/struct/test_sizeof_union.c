5ab1d5e45e20bae3a173d1f0da1762ea29b6ae4f
Paul Gey <narpfel@gmx.de>
2026-03-23 20:10:26 +0100

    add `known-bug` test for `sizeof` of unions

---
+++ panko/tests/cases/execute/struct/test_sizeof_union.c
@@ -0,0 +1,18 @@
+// [[known-bug]]
+
+int printf(char const*, ...);
+
+union U {
+    int x[2];
+    char c;
+};
+
+union U2 {
+    char c;
+    int x[3];
+};
+
+int main() {
+    // [[print: 8 12]]
+    printf("%zu %zu\n", sizeof(union U), sizeof(union U2));
+}
