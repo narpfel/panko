cbbe33ed6b45149337067a062c61cc314864d2ed
Paul Gey <narpfel@gmx.de>
2026-04-04 21:04:13 +0200

    add test for assignments to bitfield members

---
+++ panko/tests/cases/execute/struct/bitfield/test_assign_to_bitfield.c
@@ -0,0 +1,27 @@
+int printf(char const*, ...);
+
+struct T {
+    unsigned int x : 20;
+    long y : 20;
+};
+
+int main() {
+    union {
+        struct T t;
+        long x;
+    } u = {};
+    u.t.x = 0x55555;
+    u.t.y = 0xbbbbb;
+    // [[print: 0x000000bbbbb55555]]
+    printf("0x%016lx\n", u.x);
+
+    unsigned assign_to_x = u.t.x = 0xabcde;
+    // [[print: 0x000000bbbbbabcde 0x000abcde 0x000abcde]]
+    printf("0x%016lx 0x%08x 0x%08x\n", u.x, u.t.x, assign_to_x);
+
+    // FIXME: the assignment expression evaluates to the bitfield’s bits, but it
+    // should evaluate to the sign-extended bits
+    long assign_to_y = u.t.y = 0xcba98;
+    // [[print: 0x000000cba98abcde 0xfffcba98 0x000cba98]]
+    printf("0x%016lx 0x%08x 0x%08x\n", u.x, (int)u.t.y, (int)assign_to_y);
+}
