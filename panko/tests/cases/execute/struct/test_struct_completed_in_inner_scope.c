d2a1696e7b61b03c33822ff95fbabc820ef13efc
Paul Gey <narpfel@gmx.de>
2026-04-26 20:27:59 +0200

    add `known-bug` test for completing a forward declared struct in an inner scope

---
+++ panko/tests/cases/execute/struct/test_struct_completed_in_inner_scope.c
@@ -0,0 +1,12 @@
+// [[known-bug]]
+
+int main() {
+    struct T;
+    {
+        struct T {
+            int x;
+        };
+    }
+    // [[compile-error: cannot declare variable `incomplete_here` with incomplete type `struct T~2`]]
+    struct T incomplete_here = {.x = 42};
+}
