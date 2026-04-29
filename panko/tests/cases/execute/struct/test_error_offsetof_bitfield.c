e772a1eafd7623209c0be6108ce16d43792eccba
Paul Gey <narpfel@gmx.de>
2026-04-29 21:02:27 +0200

    add `known-bug` test for `offsetof` of bitfield member

---
+++ panko/tests/cases/execute/struct/test_error_offsetof_bitfield.c
@@ -0,0 +1,16 @@
+// [[known-bug]]
+
+#include <stddef.h>
+
+struct T {
+    int x:20;
+    int y;
+};
+
+int main() {
+    // invalid
+    // [[compile-error: cannot apply `offsetof` to bitfield member `x`]]
+    offsetof(struct T, x);
+    // but this is okay
+    offsetof(struct T, y);
+}
