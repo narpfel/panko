15ed6267bdb212b52ac90c7b7874087f6e74919e
Paul Gey <narpfel@gmx.de>
2026-05-05 22:27:36 +0200

    add tests for `__func__`

---
+++ panko/tests/cases/execute/functions/test_builtin_name___func__.c
@@ -0,0 +1,19 @@
+int printf(char const*, ...);
+
+#define PRINT_FUNC() printf("%s %zu %d\n", __func__, sizeof __func__, __func__ == __func__)
+
+void function() {
+    PRINT_FUNC();
+}
+
+int main() {
+    // [[print: function 9 1]]
+    function();
+
+    // [[print: main 5 1]]
+    PRINT_FUNC();
+
+    char const* func = __func__;
+    // [[print: 1]]
+    printf("%d\n", func == __func__);
+}
---
+++ panko/tests/cases/functions/test_error___func___outside_function.c
@@ -0,0 +1,1 @@
+char const* func = __func__;
