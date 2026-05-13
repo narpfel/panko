ee6d22a4a682a9ba13835276910e349ece24677c
Paul Gey <narpfel@gmx.de>
2026-05-13 21:48:41 +0200

    fix typos in test assertions

--- panko/tests/cases/execute/initialisation/test_braced_initialisation_of_array_of_array_of_char_ptr_with_string_literal.c
+++ panko/tests/cases/execute/initialisation/test_braced_initialisation_of_array_of_array_of_char_ptr_with_string_literal.c
@@ -1,11 +1,10 @@
-// [[known-bug]]
 // [[return: 1]]
 
 int printf(char const*, ...);
 
 int main() {
     typeof(typeof(char const*)[1]) s[] = {"hello world"};
-    // [[print: hello world])
+    // [[print: hello world]]
     printf("%s\n", s[0][0]);
     return _Lengthof(s);
 }
--- panko/tests/cases/execute/initialisation/test_braced_initialisation_of_array_of_char_ptr_with_string_literal.c
+++ panko/tests/cases/execute/initialisation/test_braced_initialisation_of_array_of_char_ptr_with_string_literal.c
@@ -1,11 +1,10 @@
-// [[known-bug]]
 // [[return: 1]]
 
 int printf(char const*, ...);
 
 int main() {
     typeof(char const*) s[] = {"hello world"};
-    // [[print: hello world])
+    // [[print: hello world]]
     printf("%s\n", s[0]);
     return _Lengthof(s);
 }
