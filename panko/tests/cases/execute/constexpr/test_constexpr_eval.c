18bb30089f67fcbdbf3936f3afb21a452674fbc4
Paul Gey <narpfel@gmx.de>
2026-06-01 21:54:49 +0200

    constexpr: add some more tests for integral binops

--- panko/tests/cases/execute/constexpr/test_constexpr_eval.c
+++ panko/tests/cases/execute/constexpr/test_constexpr_eval.c
@@ -4,4 +4,20 @@ int main() {
     static unsigned n = 1u << 31;
     // [[print: 1]]
     printf("%d\n", n == (1u << 31));
+
+    static int a = 0x12345 >> 4;
+    // [[print: 0x1234]]
+    printf("0x%x\n", a);
+
+    static int b = 0b101010 & 0b111;
+    // [[print: 0b10]]
+    printf("0b%b\n", b);
+
+    static int c = 0b101010 | 0b111;
+    // [[print: 0b101111]]
+    printf("0b%b\n", c);
+
+    static int d = 0b101010 ^ 0b111;
+    // [[print: 0b101101]]
+    printf("0b%b\n", d);
 }
