8e2a13768ad982c2c94dc7346bdbe16b5bef359d
Paul Gey <narpfel@gmx.de>
2026-06-09 20:46:21 +0200

    add `known-bug` test for addressof of compound literal with `static` storage class

---
+++ panko/tests/cases/execute/constexpr/test_addressof_static_compound_literal.c
@@ -0,0 +1,15 @@
+// [[known-bug]]
+
+int printf(char const*, ...);
+
+struct T {
+    int x;
+    int y;
+};
+
+int main() {
+    static struct T* p = &(static struct T){1, 2};
+
+    // [[print 1 2]]
+    printf("%d %d\n", p->x, p->y);
+}
