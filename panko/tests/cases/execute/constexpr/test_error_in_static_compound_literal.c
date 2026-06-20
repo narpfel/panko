1b1c6502e707a8e3551b8fb6b45304ffe21121e3
Paul Gey <narpfel@gmx.de>
2026-06-20 20:53:55 +0200

    constexpr: add `known-bug` test for error recovery from errors in static compound literals

---
+++ panko/tests/cases/execute/constexpr/test_error_in_static_compound_literal.c
@@ -0,0 +1,8 @@
+// [[nosnapshot]]
+// [[known-bug]]
+
+int main() {
+    // [[compile-error: division by zero in constant expression]]
+    // [[compile-error: signed overflow in constant expression]]
+    static int x = (static int){1 / 0} + (static int){0x7fff'ffff + 1};
+}
