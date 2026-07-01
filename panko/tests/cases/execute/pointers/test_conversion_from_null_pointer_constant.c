b461a266d161026cb48965c50cc0879c80ea1268
Paul Gey <narpfel@gmx.de>
2026-07-01 21:43:32 +0200

    add `known-bug` test for conversion of null pointer constant to pointer type

---
+++ panko/tests/cases/execute/pointers/test_conversion_from_null_pointer_constant.c
@@ -0,0 +1,11 @@
+// [[return: 1]]
+// [[known-bug]]
+
+int main() {
+    int* a = 0;
+    bool* b = false;
+    char const* c = (void*)0;
+    long* const d = nullptr;
+    typeof(int())* e = 42 ? 0 : 1;
+    return !a && !b && !c && !d && !e;
+}
