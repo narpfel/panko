4a80a35939dcf24d9876572bac48a6591f5b3040
Paul Gey <narpfel@gmx.de>
2026-08-01 23:35:48 +0200

    fix `return` assertion in failing test to actually assert an exit code

--- panko/tests/cases/execute/struct/test_declaration_in_typeof_is_not_ignored.c
+++ panko/tests/cases/execute/struct/test_declaration_in_typeof_is_not_ignored.c
@@ -1,9 +1,9 @@
+// [[return: 42]]
 // [[known-bug: struct decl contained in `typeof` without declarators is ignored]]
 
 typeof(struct T { int x; });
 
 int main() {
     struct T x = {.x = 42};
-    // [[return: 42]]
     return x.x;
 }
