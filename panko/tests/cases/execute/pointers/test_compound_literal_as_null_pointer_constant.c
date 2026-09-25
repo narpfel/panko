72435f87730817f90b414627fae8c3b0be479499
Paul Gey <narpfel@gmx.de>
2026-09-25 20:25:35 +0200

    fix TODO comment in test for using a compound literal as a nullpointer constant

--- panko/tests/cases/execute/pointers/test_compound_literal_as_null_pointer_constant.c
+++ panko/tests/cases/execute/pointers/test_compound_literal_as_null_pointer_constant.c
@@ -1,7 +1,7 @@
 // TODO: Should treating compound literals as (integral) constant expressions
-// be allowed? GCC disallows this, and clang is somewhat inconsistent in that
-// they’re constant expressions that can be used in e. g. initialisers for
-// statics but not as nullptr constants.
+// be allowed? GCC (with a warning) and clang (without a warning) (somewhat
+// inconsistently?) allow them as constant expressions that can be used in e. g.
+// initialisers for statics but not as nullptr constants.
 
 // [[return: 1]]
 
